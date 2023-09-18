9bd8fb88864c546a1042bc6ab0c1b8ae80043050
Sai Sindu <saisindu9398640181@gmail.com>
2023-09-18 11:09:44 +0000

    Blog List App

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import './index.css'
+
+const BlogItem = props => {
+  const {blogDetails} = props
+  const {title, description, publishedDate} = blogDetails
+  return (
+    <li className="blogItem-card">
+      <div className="post-card">
+        <h1 className="title">{title}</h1>
+        <p className="published-date">{publishedDate}</p>
+      </div>
+      <p className="description">{description}</p>
+      <hr className="separator" />
+    </li>
+  )
+}
+export default BlogItem
---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-card">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="user-img"
+    />
+    <h1 className="name">Wade Warren</h1>
+    <p className="designation">software developer at Uk</p>
+  </div>
+)
+export default UserInfo
